7c46e62349300ababb89d33aa26c6cb91ce23114
Jerry Wu <Jerry@Jerrys-Mac.local>
2013-09-25 12:08:43 +1000

    123

---
+++ RedBourne_IOS/Medication.h
@@ -0,0 +1,19 @@
+//
+//  Medication.h
+//  RedBourne_IOS
+//
+//  Created by Jerry on 25/09/13.
+//  Copyright (c) 2013 Qodo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Medication : NSObject
+
+@property (strong, nonatomic) NSString *name;
+
+-(id)initWithName:(NSString *)aName;
+
+
+
+@end
